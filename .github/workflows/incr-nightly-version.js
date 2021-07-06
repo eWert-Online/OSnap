fb1697b248b1dd93e0559cd1298a0925a54779c2
Torben Ewert <torben@ewert-online.com>
2021-07-06 20:31:16 +0200

    ci: set nightly build number to commit hash

---
+++ .github/workflows/incr-nightly-version.js
@@ -0,0 +1,20 @@
+const { execSync } = require("child_process");
+const path = require("path");
+const fs = require("fs-extra");
+
+const args = process.argv.slice(2);
+
+let packageJsonPath = path.join(__dirname, "../../package.json");
+if (args[0] != null) {
+  packageJsonPath = path.resolve(args[0]);
+}
+
+const pkgJsonData = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+
+const commit = execSync("git rev-parse --verify HEAD").toString();
+const version = `${pkgJsonData.version}-nightly.${commit.slice(0, 6)}`;
+
+pkgJsonData.version = version;
+fs.writeFileSync(packageJsonPath, JSON.stringify(pkgJsonData, null, 2));
+
+console.log(pkgJsonData.version);
