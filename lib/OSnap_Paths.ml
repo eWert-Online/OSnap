d8a46a65fc4b352482f5a428b1fdc9bc646e7cd7
Torben Ewert <Torben@eWert-Online.com>
2023-01-07 16:03:19 +0100

    fix: allow global config files with a name other than "osnap.config.yaml" (#29)

--- lib/OSnap_Paths.ml
+++ lib/OSnap_Paths.ml
@@ -1,20 +1,20 @@
 let get_snapshot_root_path (config : OSnap_Config.Types.global) =
-  config.root_path ^ config.snapshot_directory
+  Filename.concat config.root_path config.snapshot_directory
 ;;
 
 let get_base_images_dir (config : OSnap_Config.Types.global) =
   let base_path = get_snapshot_root_path config in
-  base_path ^ "/__base_images__"
+  Filename.concat base_path "__base_images__"
 ;;
 
 let get_updated_dir (config : OSnap_Config.Types.global) =
   let base_path = get_snapshot_root_path config in
-  base_path ^ "/__updated__"
+  Filename.concat base_path "__updated__"
 ;;
 
 let get_diff_dir (config : OSnap_Config.Types.global) =
   let base_path = get_snapshot_root_path config in
-  base_path ^ "/__diff__"
+  Filename.concat base_path "__diff__"
 ;;
 
 type t =
