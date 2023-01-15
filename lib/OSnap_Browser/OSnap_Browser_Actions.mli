d136bb250709f7fc2001eeb9d19eb277058bffbb
Torben Ewert <torben@ewert-online.com>
2023-01-15 19:46:44 +0100

    feat: don't fail on missing or invisible selectors

--- lib/OSnap_Browser/OSnap_Browser_Actions.mli
+++ lib/OSnap_Browser/OSnap_Browser_Actions.mli
@@ -17,14 +17,22 @@ val type_text
   -> selector:string
   -> text:string
   -> OSnap_Browser_Target.target
-  -> (unit, [> `OSnap_CDP_Protocol_Error of string ]) Lwt_result.t
+  -> ( unit
+     , [> `OSnap_CDP_Protocol_Error of string
+       | `OSnap_Selector_Not_Found of string
+       | `OSnap_Selector_Not_Visible of string
+       ] )
+     Lwt_result.t
 
 val get_quads_all
   :  document:Cdp.Commands.DOM.GetDocument.Response.result
   -> selector:string
   -> OSnap_Browser_Target.target
   -> ( ((float * float) * (float * float)) list
-     , [> `OSnap_CDP_Protocol_Error of string ] )
+     , [> `OSnap_CDP_Protocol_Error of string
+       | `OSnap_Selector_Not_Found of string
+       | `OSnap_Selector_Not_Visible of string
+       ] )
      Lwt_result.t
 
 val get_quads
@@ -32,27 +40,45 @@ val get_quads
   -> selector:string
   -> OSnap_Browser_Target.target
   -> ( (float * float) * (float * float)
-     , [> `OSnap_CDP_Protocol_Error of string ] )
+     , [> `OSnap_CDP_Protocol_Error of string
+       | `OSnap_Selector_Not_Found of string
+       | `OSnap_Selector_Not_Visible of string
+       ] )
      Lwt_result.t
 
 val mousemove
   :  document:Cdp.Commands.DOM.GetDocument.Response.result
   -> to_:[< `Coordinates of Cdp.Types.number * Cdp.Types.number | `Selector of string ]
   -> OSnap_Browser_Target.target
-  -> (unit, [> `OSnap_CDP_Protocol_Error of string ]) Lwt_result.t
+  -> ( unit
+     , [> `OSnap_CDP_Protocol_Error of string
+       | `OSnap_Selector_Not_Found of string
+       | `OSnap_Selector_Not_Visible of string
+       ] )
+     Lwt_result.t
 
 val click
   :  document:Cdp.Commands.DOM.GetDocument.Response.result
   -> selector:string
   -> OSnap_Browser_Target.target
-  -> (unit, [> `OSnap_CDP_Protocol_Error of string ]) Lwt_result.t
+  -> ( unit
+     , [> `OSnap_CDP_Protocol_Error of string
+       | `OSnap_Selector_Not_Found of string
+       | `OSnap_Selector_Not_Visible of string
+       ] )
+     Lwt_result.t
 
 val scroll
   :  document:Cdp.Commands.DOM.GetDocument.Response.result
   -> selector:string option
   -> px:int option
   -> OSnap_Browser_Target.target
-  -> (unit, [> `OSnap_CDP_Protocol_Error of string ]) Lwt_result.t
+  -> ( unit
+     , [> `OSnap_CDP_Protocol_Error of string
+       | `OSnap_Selector_Not_Found of string
+       | `OSnap_Selector_Not_Visible of string
+       ] )
+     Lwt_result.t
 
 val set_size
   :  width:Cdp.Types.number
