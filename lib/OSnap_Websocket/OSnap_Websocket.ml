25a53614044aed04464989d5cfac441cfb996784
Torben Ewert <torben@ewert-online.com>
2024-07-13 19:59:52 +0200

    feat: implement download and websocket with httpun

--- lib/OSnap_Websocket/OSnap_Websocket.ml
+++ lib/OSnap_Websocket/OSnap_Websocket.ml
@@ -7,7 +7,6 @@ let id () =
   !id
 ;;
 
-let close_requests = Queue.create ()
 let pending_requests = Queue.create ()
 let sent_requests = Hashtbl.create 10
 let listeners = Hashtbl.create 10
@@ -20,52 +19,42 @@ let call_event_handlers key message =
       Hashtbl.remove events key))
 ;;
 
-let connect ~sw ~env url =
-  let uri = Uri.of_string url in
-  let resource = Uri.path uri in
-  let*? client =
-    Piaf.Client.create env ~sw (Uri.with_scheme uri (Some "http"))
-    |> Result.map_error (fun _ -> `OSnap_CDP_Connection_Failed)
+let websocket_handler ~sw u wsd =
+  let send_payload payload =
+    let payload = Bytes.of_string payload in
+    let len = Bytes.length payload in
+    Httpun_ws.Wsd.send_bytes wsd ~kind:`Text payload ~off:0 ~len
   in
-  let*? wsd =
-    Piaf.Client.ws_upgrade client resource
-    |> Result.map_error (fun _ -> `OSnap_CDP_Connection_Failed)
-  in
-  let close () = Piaf.Ws.Descriptor.close wsd in
   let rec input_loop () =
     let () = Eio.Fiber.yield () in
-    if not (Queue.is_empty close_requests)
-    then (
-      close_requests |> Queue.iter (fun resolver -> Eio.Promise.resolve resolver ());
-      close_requests |> Queue.clear;
-      close ())
-    else if not (Queue.is_empty pending_requests)
+    if not (Queue.is_empty pending_requests)
     then (
       let key, message, resolver = Queue.take pending_requests in
-      Piaf.Ws.Descriptor.send_string wsd message;
+      let () = send_payload message in
       Hashtbl.add sent_requests key resolver;
       input_loop ())
     else input_loop ()
   in
-  Result.ok
-  @@ Eio.Fiber.fork ~sw
-  @@ fun () ->
-  Eio.Fiber.both
-    (fun () ->
-      input_loop ();
-      Piaf.Client.shutdown client)
-    (fun () ->
-      wsd
-      |> Piaf.Ws.Descriptor.messages
-      |> Piaf.Stream.iter ~f:(fun (_opcode, { Piaf.IOVec.buffer; off; len }) ->
-        let response = Bigstringaf.substring ~off ~len buffer in
-        let json = response |> Yojson.Safe.from_string in
-        let id = json |> Yojson.Safe.Util.member "id" |> Yojson.Safe.Util.to_int_option in
+  let frame ~(opcode : Httpun_ws.Websocket.Opcode.t) ~is_fin:_ ~len payload =
+    match opcode with
+    | `Connection_close | `Continuation | `Other _ -> ()
+    | `Ping -> Httpun_ws.Wsd.send_pong wsd
+    | `Pong -> ()
+    | `Text | `Binary ->
+      let buf = Eio.Buf_write.create len in
+      let on_eof () =
+        let response = Eio.Buf_write.serialize_to_string buf in
+        let json = response |> Yojson.Basic.from_string in
+        let id =
+          json |> Yojson.Basic.Util.member "id" |> Yojson.Basic.Util.to_int_option
+        in
         let method_ =
-          json |> Yojson.Safe.Util.member "method" |> Yojson.Safe.Util.to_string_option
+          json |> Yojson.Basic.Util.member "method" |> Yojson.Basic.Util.to_string_option
         in
         let sessionId =
-          json |> Yojson.Safe.Util.member "sessionId" |> Yojson.Safe.Util.to_string_option
+          json
+          |> Yojson.Basic.Util.member "sessionId"
+          |> Yojson.Basic.Util.to_string_option
         in
         (match method_, sessionId with
          | None, None -> ()
@@ -86,7 +75,61 @@ let connect ~sw ~env url =
           Hashtbl.find_opt sent_requests key
           |> Option.iter (fun resolver -> Eio.Promise.resolve resolver response);
           Hashtbl.remove sent_requests key;
-          ()))
+          ()
+      in
+      let rec on_read bs ~off ~len:read_len =
+        let response = Bigstringaf.substring ~off ~len:read_len bs in
+        Eio.Buf_write.string buf response;
+        Eio.Fiber.yield ();
+        if len > read_len
+        then Httpun_ws.Payload.schedule_read payload ~on_read ~on_eof
+        else on_eof ()
+      in
+      Httpun_ws.Payload.schedule_read payload ~on_read ~on_eof
+  in
+  Eio.Fiber.fork_daemon ~sw input_loop;
+  let eof () = Eio.Promise.resolve u () in
+  Httpun_ws.Websocket_connection.{ frame; eof }
+;;
+
+let error_handler = function
+  | `Handshake_failure (rsp, _body) ->
+    Format.eprintf "Handshake failure: %a\n%!" Httpun.Response.pp_hum rsp
+  | _ -> assert false
+;;
+
+let connect ~sw ~env url =
+  let net = Eio.Stdenv.net env in
+  Eio.Fiber.fork_daemon ~sw (fun () ->
+    let uri = Uri.of_string url in
+    let host = Uri.host_with_default uri ~default:"localhost" in
+    let port = Uri.port uri |> Option.value ~default:80 in
+    let addresses =
+      Eio.Net.getaddrinfo net ~service:"http" host
+      |> List.filter_map
+         @@ function
+         | `Tcp (addr, _port) ->
+           let addr = Eio.Net.Ipaddr.fold ~v4:Option.some ~v6:(Fun.const None) addr in
+           Option.map (fun addr -> `Tcp (addr, port)) addr
+         | _ -> None
+    in
+    let socket = Eio.Net.connect ~sw net (List.hd addresses) in
+    let p, u = Eio.Promise.create () in
+    let nonce = "0123456789ABCDEF" in
+    let resource = Uri.path_and_query uri in
+    let _client =
+      Httpun_ws_eio.Client.connect
+        socket
+        ~sw
+        ~nonce
+        ~host
+        ~port
+        ~resource
+        ~error_handler
+        ~websocket_handler:(websocket_handler ~sw u)
+    in
+    Eio.Promise.await p;
+    `Stop_daemon)
 ;;
 
 let send message =
@@ -110,10 +153,4 @@ let listen ?(look_behind = true) ~event ~sessionId handler =
       handler event (fun () ->
         Hashtbl.remove listeners key;
         Hashtbl.remove events key))
-;;
-
-let close () =
-  let p, resolver = Eio.Promise.create () in
-  close_requests |> Queue.add resolver;
-  Eio.Promise.await p
 ;;
--- lib/OSnap_Websocket/OSnap_Websocket.mli
+++ lib/OSnap_Websocket/OSnap_Websocket.mli
@@ -5,11 +5,5 @@ val listen
   -> (string -> (unit -> unit) -> unit)
   -> unit
 
-val close : unit -> unit
 val send : (int -> string) -> string
-
-val connect
-  :  sw:Eio.Switch.t
-  -> env:Eio_unix.Stdenv.base
-  -> string
-  -> (unit, [> `OSnap_CDP_Connection_Failed ]) result
+val connect : sw:Eio.Switch.t -> env:Eio_unix.Stdenv.base -> string -> unit
