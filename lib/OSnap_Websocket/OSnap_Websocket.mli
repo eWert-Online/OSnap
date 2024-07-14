2e30c163147f42199a26f7970b9c4c8dc9e47679
Torben Ewert <torben@ewert-online.com>
2024-07-14 13:44:22 +0200

    chore: implement websocket handler with piaf

--- lib/OSnap_Websocket/OSnap_Websocket.mli
+++ lib/OSnap_Websocket/OSnap_Websocket.mli
@@ -6,4 +6,9 @@ val listen
   -> unit
 
 val send : (int -> string) -> string
-val connect : sw:Eio.Switch.t -> env:Eio_unix.Stdenv.base -> string -> unit
+
+val connect
+  :  sw:Eio.Switch.t
+  -> env:Eio_unix.Stdenv.base
+  -> string
+  -> (unit, [> `OSnap_CDP_Connection_Failed ]) result
