0e9bf8749ddafd2d8b8943cbbd2a20fe083ae802
Torben Ewert <torben@ewert-online.com>
2024-09-21 20:01:04 +0200

    fix: initialize mirage crypto rng

--- bin/Main.ml
+++ bin/Main.ml
@@ -148,6 +148,8 @@ let default_cmd =
     handle_response
     @@ Eio_main.run
     @@ fun env ->
+    Mirage_crypto_rng_eio.run (module Mirage_crypto_rng.Fortuna) env
+    @@ fun () ->
     Lwt_eio.with_event_loop ~clock:(Eio.Stdenv.clock env)
     @@ fun _ -> Eio.Switch.run @@ fun sw -> run ~sw ~env
   in
